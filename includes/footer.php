6d84ed4b70bc1b493f72adec3495bb2eeb86718c
thisispintu <149798028+thisispintu@users.noreply.github.com>
2023-11-17 16:38:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2023 Online Course Registration,TEZPUR UNIVERSITY
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <style> .col-md-12{text-align: center;} </style>
